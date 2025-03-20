9763e5674194b74f3e32fa0bd0d48ea27ec1a71e
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2025-03-20 20:45:26 -0300

    Delete ex1_range_ac2 directory

--- ex1_range_ac2/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ex1_range_ac2',
-  webDir: 'www'
-};
-
-export default config;
