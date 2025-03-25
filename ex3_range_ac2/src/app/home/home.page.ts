216e641bf033cbdb5ba82f283af86804168564d5
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2025-03-25 13:06:00 -0300

    Add files via upload

---
+++ ex3_range_ac2/src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() { }
+
+  message: any = ''
+  valueHeight: any = 0
+  valueWeight: any = 0
+
+  onHeightChange(event: RangeCustomEvent) {
+    this.valueHeight = event.detail.value
+  }
+
+  onWeightChange(event: RangeCustomEvent) {
+    this.valueWeight = event.detail.value
+  }
+
+  calcImc() {
+    const imc = this.valueWeight / ((this.valueHeight / 100) ** 2)
+    this.message = this.getImcMessage(imc)
+  }
+
+  getImcMessage(imc: any): any {
+    if (imc < 18.5) return `Seu IMC é ${imc.toFixed(2)} e você está abaixo do peso`
+    if (imc < 24.9) return `Seu IMC é ${imc.toFixed(2)} e você está no peso normal`
+    if (imc < 29.9) return `Seu IMC é ${imc.toFixed(2)} e você está com sobrepeso`
+    if (imc < 34.9) return `Seu IMC é ${imc.toFixed(2)} e você está com obesidade`
+  }
+}
